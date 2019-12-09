4a70f331b116dbeff0a0d6b2f55f0aa2fe7d5727
Justin <zhou78620051@126.com>
2019-12-09 18:05:15 +0800

    fix: folder name

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 # !/bin/bash
 
+FULL_FOLDER=`pwd`
+FOLDER_NAME=`basename ${FULL_FOLDER}`
+
 docker-compose up -d --build &&
-docker cp leanote_server_1:/usr/local/leanote/mongodb_backup/ ./conf &&
+docker cp ${FOLDER_NAME}_server_1:/usr/local/leanote/mongodb_backup/ ./conf &&
 docker-compose exec mongodb mongorestore -h localhost -d leanote --dir /init_data
